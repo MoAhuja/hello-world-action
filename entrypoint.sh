37d4d262c271081fb06f2789ed42358c66912e01
Mohit Ahuja <mohahuja@microsoft.com>
2020-11-20 16:24:07 -0500

    Adding file parsing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,16 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+echo "Working space == "
+echo $GITHUB_WORKSPACE
+
+input="./test.json"
+while IFS= read -r line
+do
+  echo "$line"
+done < "$input"
+
+
+
+
